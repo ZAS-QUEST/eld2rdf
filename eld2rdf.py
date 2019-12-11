d583e0356614f4b9717c57cfe3e450591ae41537
Sebastian Nordhoff <nordhoff@leibniz-zas.de>
2019-12-11 12:09:09 +0100

    resolve ELAR locations as part of

--- eld2rdf.py
+++ eld2rdf.py
@@ -1,13 +1,25 @@
 import json
 from rdflib import Graph, Literal, BNode, Namespace, RDF, RDFS, URIRef
-from rdflib.namespace import DC, FOAF
+from rdflib.namespace import DC, FOAF 
 from rdflib.namespace import Namespace, NamespaceManager
 
 quest = Namespace("http://zasquest.org/")
-paradisec = Namespace("http://paradisec.org/")
+questresolver = Namespace("http://zasquest.org/resolver/")
+paradisec = Namespace("https://catalog.paradisec.org.au/collections/")
+elar = Namespace("https://lat1.lis.soas.ac.uk/corpora/ELAR/")
+elareafs = Namespace("https://elar.soas.ac.uk/resources/")
+dbpedia = Namespace("http://dbpedia.org/ontology/")
 namespace_manager = NamespaceManager(Graph())
 namespace_manager.bind('paradisec', paradisec)
 namespace_manager.bind('quest', quest)
+namespace_manager.bind('questresolver', questresolver)
+namespace_manager.bind('dbpedia', dbpedia)
+
+archive_namespaces = {
+    'paradisec': paradisec,
+    'elar': elar,
+    'elareafs': elareafs
+    }
 
 g = Graph(namespace_manager = namespace_manager) 
 g.bind("rdfs", RDFS)
@@ -18,17 +30,25 @@ translation_dictionary = json.loads(open('translations.json').read())
  
 LIMIT = 9999999 
 for filename in list(translation_dictionary.keys())[:LIMIT]: 
+    fileID = filename.split('/')[-1]
+    filehash = 'x'+hex(hash("%s"%filename.split('/')[-1]))
     for tiertype in translation_dictionary[filename]: 
         tierIDs = translation_dictionary[filename][tiertype] 
         for tierID in tierIDs:
             tier = translation_dictionary[filename][tiertype][tierID]
-            output_tierID = tierID.replace(" ",'')
-            #g.add((paradisec[tierID], RDF.type, quest.Tier))
-            #g.add((paradisec[tierID], RDFS.label, " ".join(tier)))
+            output_tierID = "%s_%s" %(filehash, tierID.replace(" ",''))
+            g.add((questresolver[output_tierID], RDF.type, quest.Tier)) 
+            archive_namespace = 'paradisec'
+            resolveID = fileID
+            if filename.startswith('elareafs'):
+                archive_namespace = 'elareafs' #we hackishly infer the archive from the filename TODO 
+                resolveID = fileID.replace('-b-','/') #better use landing page instead of file location
+            g.add((questresolver[output_tierID], dbpedia.isPartOf, archive_namespaces[archive_namespace][resolveID]))   
             for j, annotation in enumerate(tier):
-                rdfID = "x%s_%s_%s"%(filename.split('/')[-1][:11],output_tierID.replace('@','_'),j) 
-                g.add( ( paradisec[rdfID], RDF.type, quest.Annotation))
-                g.add( ( paradisec[rdfID], RDFS.label, Literal('%s'%annotation.strip())))
+                rdfID = "%s_%s"%(output_tierID,j) 
+                g.add( ( questresolver[rdfID], RDF.type, quest.Annotation))
+                g.add( ( questresolver[rdfID], dbpedia.isPartOf, questresolver[output_tierID]))                
+                g.add( ( questresolver[rdfID], RDFS.label, Literal('%s'%annotation.strip())))
                 
 #for s,p,o in g:
     #print(s,p,o)
