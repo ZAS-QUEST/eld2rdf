7fded6f90a8e2fd3f56c473776326c5f4ffbaee2
Sebastian Nordhoff <nordhoff@leibniz-zas.de>
2019-12-19 11:41:39 +0100

    pylint

--- nerd2rdf.py
+++ nerd2rdf.py
@@ -1,47 +1,31 @@
+""" transform a json file mapping files to wikidata IDs to JSON"""
+
 import json
-import glob
 import sys
-from rdflib import Namespace, Graph, Literal, RDF, RDFS #, URIRef, BNode
-from rdflib.namespace import NamespaceManager, DC #, FOAF
+from rdflib import Namespace, Graph, RDFS  # , URIRef, BNode
+from rdflib.namespace import NamespaceManager, DC  # , FOAF
 
 
-#{
-    #"anlaeafs/18106-01f1SylviaJackson.eaf": {
-        #"Q108266": "Navajos",
-        #"Q2934": "goat",
-        #"Q780": "chicken",
-        #"Q89": "apple",
-        #"Q96": "Mexican"
-    #},
-    #"anlaeafs/18106-02f1WarlanceChee.eaf": {
-        #"Q108266": "Navajo",
-        #"Q13310": "Navajo language",
-        #"Q266750": "medicine man",
-        #"Q2934": "goat",
-        #"Q65358": "Gallup",
-        #"Q96": "Mexican"
-    #},
-    
-    
-
-#define general namespaces
+# define general namespaces
 QUEST = Namespace("http://zasquest.org/")
-QUESTRESOLVER = Namespace("http://zasquest.org/resolver/") 
+QUESTRESOLVER = Namespace("http://zasquest.org/resolver/")
 WIKIDATA = Namespace("https://www.wikidata.org/wiki/")
 
-#define archive namespaces
+# define archive namespaces
 NAMESPACE_MANAGER = NamespaceManager(Graph())
-NAMESPACE_MANAGER.bind('wikidata', WIKIDATA)
-NAMESPACE_MANAGER.bind('quest', QUEST) #for ontology
-NAMESPACE_MANAGER.bind('QUESTRESOLVER', QUESTRESOLVER) #for the bridge for rewritable URLs
+NAMESPACE_MANAGER.bind("wikidata", WIKIDATA)
+NAMESPACE_MANAGER.bind("quest", QUEST)  # for ontology
+NAMESPACE_MANAGER.bind(
+    "QUESTRESOLVER", QUESTRESOLVER
+)  # for the bridge for rewritable URLs
 NAMESPACE_MANAGER.bind("rdfs", RDFS)
 NAMESPACE_MANAGER.bind("dc", DC)
 
 ARCHIVE_NAMESPACES = {
-    'paradisec': Namespace("https://cataloGRAPH.paradisec.orGRAPH.au/collections/"),
-    'elarcorpus': Namespace("https://lat1.lis.soas.ac.uk/corpora/ELAR/"),
-    'elarfiles': Namespace("https://elar.soas.ac.uk/resources/")
-    }
+    "paradisec": Namespace("https://cataloGRAPH.paradisec.orGRAPH.au/collections/"),
+    "elarcorpus": Namespace("https://lat1.lis.soas.ac.uk/corpora/ELAR/"),
+    "elarfiles": Namespace("https://elar.soas.ac.uk/resources/"),
+}
 
 for archive in ARCHIVE_NAMESPACES:
     NAMESPACE_MANAGER.bind(archive, ARCHIVE_NAMESPACES[archive])
@@ -49,16 +33,14 @@ for archive in ARCHIVE_NAMESPACES:
 
 if __name__ == "__main__":
     GRAPH = Graph(namespace_manager=NAMESPACE_MANAGER)
-    filename = sys.argv[1]
-    with open(filename) as jsoninfile:
-        nerdjson = json.loads(jsoninfile.read())
-        
-    for eaffile in nerdjson:
-        for wikidataID in nerdjson[eaffile]:
-            GRAPH.add((QUESTRESOLVER[eaffile],
-                        DC.topic,
-                        WIKIDATA[wikidataID]))
-   
+    FILENAME = sys.argv[1]
+    with open(FILENAME) as jsoninfile:
+        NERDJSON = json.loads(jsoninfile.read())
+
+    for eaffile in NERDJSON:
+        for wikidataID in NERDJSON[eaffile]:
+            GRAPH.add((QUESTRESOLVER[eaffile], DC.topic, WIKIDATA[wikidataID]))
+
     print("writing output")
-    with open(filename.replace('json','n3'), "wb") as rdfout:
-        rdfout.write(GRAPH.serialize(format='n3'))
+    with open(FILENAME.replace("json", "n3"), "wb") as rdfout:
+        rdfout.write(GRAPH.serialize(format="n3"))
